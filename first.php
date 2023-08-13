fe6fcfc91d8f7f9788d61a717cd0fca54367e5e4
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-08-13 21:20:23 +0530

    commit

--- first.php
+++ first.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Antarip</title>
-</head>
-<body>
+<?php
+   // $user = $_GET['user'];
+   echo "<pre>";
+   print_r($_GET);
+   
+   echo "</pre>";
+   echo $_GET['user'];
 
-    <?php
-    //    $a = 3;
-    //    echo $a + $a;
-    //print anything 
-    echo "Hello World";
-    ?> 
-
-</body>
-</html>
+    //echo $_POST['user'];
+?>
