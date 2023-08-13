023b8e23ae4d2eaf64dee868ae2de31b28090b3d
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-08-13 12:33:34 +0530

    First commit

---
+++ first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antarip</title>
+</head>
+<body>
+    <?php
+        $a = 3;
+        echo $a + $a;
+    ?>
+</body>
+</html>
